e16928d6f12beba4ce64073921664aa7c188203e
mohamedhamdy007 <mohamedhamdy007@users.noreply.github.com>
2025-10-26 20:39:40 +0300

    Update settings.py

--- wa_dj_api_intg/settings.py
+++ wa_dj_api_intg/settings.py
@@ -122,7 +122,8 @@ CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
