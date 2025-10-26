c923aa108b7072951ba58870e297875c7b74cf68
mohamedhamdy007 <mohamedhamdy007@users.noreply.github.com>
2025-10-26 16:39:23 +0300

    Update settings.py

--- wa_dj_api_intg/settings.py
+++ wa_dj_api_intg/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-py=!twp238u1$(#)auatrk(*2yi2avfa^vp-^a8ajb#(^ee=i0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','https://livekit-whatsapp-api-ggd5fcdabhdebbcd.eastus2-01.azurewebsites.net/']
 
 
 # Application definition
