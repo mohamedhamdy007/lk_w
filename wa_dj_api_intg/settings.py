a17327313c569951e1f5f8c39b8daf3fa438f48f
mohamedhamdy007 <mohamedhamdy007@users.noreply.github.com>
2025-10-26 21:31:49 +0300

    Update settings.py

--- wa_dj_api_intg/settings.py
+++ wa_dj_api_intg/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import warnings
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pydub")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
